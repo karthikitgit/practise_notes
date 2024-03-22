9555bfba82b3b5556ad689ec05677bee76e3da2c
karthikitgit <karthik1179428@gmail.com>
2024-03-22 21:55:09 +0530

    adding helper_module

--- empty.py
+++ empty.py
@@ -4,4 +4,11 @@ print("empty")
 # wc -w – prints the number of words in a file.
 # wc -c – Displays the count of bytes in a file.
 # wc -m – prints the count of characters from a file.
-# wc -L – prints only the length of the longest line in a file.L
+# wc -L – prints only the length of the longest line in a file.
+from setuptools import setup, find_packages
+
+setup(
+    name='helper_module',
+    version='0.1',
+    py_modules=['helper_module'],
+)
